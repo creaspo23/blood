dcb8292b61a9bfd2d904067c59f33ba73f943887
walieldin.hassan <creaspo6@gmail.com>
2023-03-05 10:32:56 +0200

    fix dropdown menu style

--- resources/views/all-investigations.blade.php
+++ resources/views/all-investigations.blade.php
@@ -21,7 +21,7 @@
             <div class="card">
                 <div class="card-body">
                     <div class="row mb-4">
-                        <div class="col-12">
+                        <div class="col-14">
                             <div class="page-title-box d-flex align-items-center justify-content-between">
                                 <h4 class="card-title" style="text-align: center">
 
@@ -77,7 +77,7 @@
                                         @endif
                                     </td>
                                     @foreach ($investigation->tests as $item)
-                                    
+
                                         <td>
                                             {{ $item->result }}
 
